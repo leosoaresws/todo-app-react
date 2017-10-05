58bd761863ffbca5d846e5a7840cec09423829a1
Leonardo Soares <leonardo.soares.ws@gmail.com>
2017-10-04 21:24:15 -0300

    configured backend server

---
+++ backend/src/config/server.js
@@ -0,0 +1,11 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extends: true }))
+
+server.listen(port, function() {
+    console.log(`BACKEND is running in port ${port}.`)   
+})
