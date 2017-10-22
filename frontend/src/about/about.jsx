f8d32984d10e36482aa0090f7dc03233c407a60a
Leonardo Soares <leonardo.soares.ws@gmail.com>
2017-10-21 22:58:45 -0300

    added page header todoForm and todoList (not implemented yet)

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small="Nós" />
+        <h2>Nossa Historia</h2>
+        <p>Lorem Ipsum dolor sit amet...</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem Ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem Ipsum dolor sit amet...</p>
     </div>
 )
